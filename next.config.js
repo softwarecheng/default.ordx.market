390cc65aaf3fe24c0026ad7ed3f2d10e6fcc9807
simonhub2023 <125529150+simonhub2023@users.noreply.github.com>
2024-09-20 00:19:20 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: false,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // 你可以根据需要添加更多页面
+    };
+  },
+  distDir: 'out',
+};
